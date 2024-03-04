f32362ac1619bd77df414bf237ccaa430ca2fabb
nick15431543 <nkocherin@inbox.ru>
2024-03-04 14:35:20 +0200

    second

--- hw_1/first.py
+++ hw_1/first.py
@@ -1,14 +1,6 @@
 import sys
 
-def cli(lines):
-    res = []
-    for num, i in enumerate(lines):
-        a = str(num + 1) + ' ' + i
-        res.append(a)
-    return res
-    
-
-if __name__ == "__main__":
+def cli():
     args = sys.argv
     lines = []
     if len(args) > 1:
@@ -17,6 +9,13 @@ if __name__ == "__main__":
     else:
         for line in sys.stdin:
             lines.append(line)
-    res = cli(lines)
+    res = []
+    for num, i in enumerate(lines):
+        a = str(num + 1) + ' ' + i
+        res.append(a)
     for line in res:
-        print(line.rstrip('\n'))
+            print(line.rstrip('\n'))
+
+
+if __name__ == "__main__":
+    cli()
---
+++ hw_1/second.py
@@ -0,0 +1,26 @@
+import sys
+
+def cli():
+    args = sys.argv
+    if len(args) > 2:
+        for i in range(1, len(args)):
+            print("==> ", args[i], "<==", sep='')
+            with open(args[i], 'r') as f:
+                lines = f.readlines()
+            for line in lines[-10:]:
+                print(line.rstrip('\n'))
+    elif len(args) == 2:
+        with open(args[1], 'r') as f:
+                lines = f.readlines()
+        for line in lines[-10:]:
+            print(line.rstrip('\n'))
+    else:
+        lines = []
+        for line in sys.stdin:
+            lines.append(line)
+        for line in lines[-17:]:
+             print(line.rstrip('\n'))
+
+
+if __name__ == "__main__":
+    cli()
